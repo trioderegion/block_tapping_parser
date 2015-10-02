017042cf23bb53723d9f83c190dd3326958a4c49
Matthew Haentschke <matthew.haentschke@gmail.com>
2015-10-01 22:54:29 -0400

    started work with session scoring

--- block_tapping_parser/block_tapping_parser/session.cpp
+++ block_tapping_parser/block_tapping_parser/session.cpp
@@ -1,12 +1,24 @@
 #include "session.h"
+#include "trial.h"
 
+using namespace std;
 
+const string sessionnumber ("Session");
 
-Session::Session()
+Session::Session(string header, vector<string> sessiondata)
 {
+  absolute_score_ = 0;
+  partial_score_ = 0;
 }
 
 
 Session::~Session()
 {
+}
+
+//Will loop over all provided trials in the vector,
+//score them, the score the session
+int Session::Score(string header, vector<string> trials)
+{
+  return 0;
 }
--- block_tapping_parser/block_tapping_parser/session.h
+++ block_tapping_parser/block_tapping_parser/session.h
@@ -1,15 +1,17 @@
 #pragma once
-
+#include <string>
+#include <vector>
 #include "trial.h"
+
 
 class Session
 {
 public:
-	Session();
+	Session(std::string, std::vector<std::string>);
 	~Session();
+  int Score(std::string, std::vector<std::string>);
 private:
-	Trial* trials_;
+	std::vector<Trial> trials_;
 	int partial_score_;
 	int absolute_score_;
 };
-
